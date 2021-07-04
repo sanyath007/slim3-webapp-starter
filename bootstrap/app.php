94b3238e4b0c8ad592e4f5e45abb1d4c5c041b29
sanyath007 <sanyath007@gmail.com>
2021-07-04 11:45:02 +0700

    Install related packages and edit bootstrap/app.php by add Dotenv class for using .env file. And finally add helpers function for using utiliies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,8 @@ session_start();
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__. '/../');
+$dotenv->load();
 /** 
  * ============================================================
  * App Configuration
