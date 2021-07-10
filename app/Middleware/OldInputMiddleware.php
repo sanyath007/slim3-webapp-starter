68e7f034501cfb2f5c4968cc2892f3b9359d57d2
sanyath007 <sanyath007@gmail.com>
2021-07-10 11:13:51 +0700

    Edit route in layouts/partials/navigation view and check array index before using in Middleware/OldInputMiddleware

--- app/Middleware/OldInputMiddleware.php
+++ app/Middleware/OldInputMiddleware.php
@@ -6,8 +6,10 @@ class OldInputMiddleware extends Middleware
 {
     public function __invoke($request, $response, $next)
     {
-        $this->container->view->getEnvironment()->addGlobal('old', $_SESSION['old']);
-        $_SESSION['old'] = $request->getParams();
+        if(array_key_exists('errors', $_SESSION)) {
+            $this->container->view->getEnvironment()->addGlobal('old', $_SESSION['old']);
+            $_SESSION['old'] = $request->getParams();
+        }
 
         return $next($request, $response);
     }
